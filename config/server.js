b7a8ce162452cd7516db521645ea8842ed26244d
PoMo <kaweepong.m@ku.th>
2021-08-04 17:37:37 +0700

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'e787ad21ea5d9c55ab4d0359879ae08f'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '72b429d0-f3ae-4097-8f7d-1a5dddba3181'
+};
